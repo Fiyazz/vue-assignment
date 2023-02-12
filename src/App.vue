9a5a8aea525dfdc6eb9bde9421146611904fdc9c
FIYAZ <abdusalamummulkhayr@gmail.com>
2023-02-12 23:22:33 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Recap_01_02_2023 from "./components/Recap_01_02_2023.vue";
+
+export default {
+  components: {
+    Recap_01_02_2023,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <Recap_01_02_2023 />
+</template>
+
+<style scoped></style>
+qgq-cydp-mhb
---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,42 @@
+
+
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="addColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script  >
+ 
+import { ref, reactive } from "@vue/reactivity";
+import colorManager from "../composables/ColorPicker";
+
+// const {colors, message, addColor} = colorManager();
+
+export default {
+  name: "ColorPickerGame",
+  data(){
+  return{
+      colors: ["red", "blue", "green", "yellow"],
+      message: '',
+      
+    }
+  },
+  methods: {
+    addColor() {},
+
+  },
+  setup() {
+    
+    const { colors, message, addColor } = colorManager();
+    return { colors, message, addColor };
+  },
+};
+
+</script>
+
+<style></style>
